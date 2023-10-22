fe6fe4b1caf3107533cfc761f120e806a035aa70
Rohan Rathod <rohanrathod2015@gmail.com>
2023-10-22 15:03:55 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Features from '@/Components/Features'
 import GetApp from '@/Components/GetApp'
 import Guide from '@/Components/Guide'
 import Hero from '@/Components/Hero'
-import Image from 'next/image'
 
 export default function Home() {
   return (
